b7964b0c7b704edf04cb69a76a71d5e711eeb4f6
Oleksandr Vyshnevskyi <w69956@student.wsiz.edu.pl>
2024-10-24 15:06:42 +0200

    lab1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
+
         int liczba = Convert.ToInt32(Console.ReadLine());
         if (liczba % 2 == 0) {
             Console.WriteLine("jest parzysta");
@@ -16,8 +17,63 @@ class Program
             if (i % 2 == 0) { 
                 Console.WriteLine(i);
             }
-        }
-       
+}
 
+        
+
+
+
+
+
+
+
+
+        int liczba = Convert.ToInt32(Console.ReadLine());
+        while (true)
+        {
+            Console.WriteLine("1.czy jest parzyste");
+            Console.WriteLine("2.wypisz parzyste");
+            Console.WriteLine("3.Silnia");
+            Console.WriteLine("4.Wyjście");
+
+            int wybor = Convert.ToInt32(Console.ReadLine());
+            if (wybor == 1)
+            {
+
+                if (liczba % 2 == 0)
+                {
+                    Console.WriteLine("jest parzysta");
+                }
+                else
+                {
+                    Console.WriteLine("nie jest parzysta");
+                }
+            }
+            if (wybor == 2)
+            {
+                for (int i = 0; i < liczba; i += 2)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+
+            }
+            if (wybor == 3)
+            {
+                static int silnia(int x)
+                {
+                    if (x == 0) return 1;
+                    return silnia(x - 1) * x;
+                }
+                Console.WriteLine(silnia(liczba));
+                if (wybor == 4)
+                {
+                    break;
+                }
+            }
+
+        }
     }
 }
+
