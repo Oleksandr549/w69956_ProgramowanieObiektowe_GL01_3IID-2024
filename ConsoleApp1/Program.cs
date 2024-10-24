a752fcb443232ab245780b5011c6b4a0a356dca6
Oleksandr Vyshnevskyi <w69956@student.wsiz.edu.pl>
2024-10-24 15:08:04 +0200

    lab1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,25 +5,19 @@ class Program
     static void Main()
     {
 
-        int liczba = Convert.ToInt32(Console.ReadLine());
-        if (liczba % 2 == 0) {
-            Console.WriteLine("jest parzysta");
-        }
-        else
-        {
-            Console.WriteLine("nie jest parzysta");
-        }
-        for (int i = 0; i < liczba; i++) {
-            if (i % 2 == 0) { 
-                Console.WriteLine(i);
-            }
-}
-
-        
-
-
-
-
+//        int liczba = Convert.ToInt32(Console.ReadLine());
+//        if (liczba % 2 == 0) {
+//            Console.WriteLine("jest parzysta");
+//        }
+//        else
+//        {
+//            Console.WriteLine("nie jest parzysta");
+//        }
+//        for (int i = 0; i < liczba; i++) {
+//            if (i % 2 == 0) { 
+//                Console.WriteLine(i);
+//            }
+//}
 
 
 
