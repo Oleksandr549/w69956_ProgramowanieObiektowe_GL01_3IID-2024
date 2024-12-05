725af865a5da327f5183798bfdcb0547eadcb852
Oleksandr Vyshnevsky <109508830+Oleksandr549@users.noreply.github.com>
2024-12-05 13:55:46 +0100

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,221 +1,97 @@
-﻿using System;
-class Program
+﻿namespace ConsoleApp1
 {
-    static void Main()
+    internal class Program
     {
-
-        //        int liczba = Convert.ToInt32(Console.ReadLine());
-        //        if (liczba % 2 == 0) {
-        //            Console.WriteLine("jest parzysta");
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("nie jest parzysta");
-        //        }
-        //        for (int i = 0; i < liczba; i++) {
-        //            if (i % 2 == 0) { 
-        //                Console.WriteLine(i);
-        //            }
-        //}
-
-
-
-
-        int liczba = Convert.ToInt32(Console.ReadLine());
-        while (true)
+        static void Main(string[] args)
         {
-            Console.WriteLine("1.czy jest parzyste");
-            Console.WriteLine("2.wypisz parzyste");
-            Console.WriteLine("3.Silnia");
-            Console.WriteLine("4.Wyjście");
-
-            int wybor = Convert.ToInt32(Console.ReadLine());
-            if (wybor == 1)
+            while (true)
             {
+                Console.WriteLine("Wybierz");
+                Console.WriteLine("1. Czy parzysta");
+                Console.WriteLine("2. Liczby parzyste");
+                Console.WriteLine("3. Silnia");
+                Console.WriteLine("4. Gra");
+                Console.WriteLine("0. Wyjście");
 
-                if (liczba % 2 == 0)
+                var n = Console.ReadLine();
+
+                switch (int.Parse(n))
                 {
-                    Console.WriteLine("jest parzysta");
+                    case 1:
+                        CheckEven();
+                        break;
+                    case 2:
+                        {
+                            WriteEven();
+                            break;
+                        }
+                    case 3:
+                        {
+                            var siln = Console.ReadLine();
+                            Console.WriteLine(Silnia(int.Parse(siln)));
+                            break;
+                        }
+                    case 4:
+                        Game();
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("nie jest parzysta");
-                }
-            }
-            if (wybor == 2)
-            {
-                for (int i = 0; i < liczba; i += 2)
-                {
-                    Console.WriteLine(i);
-                }
 
-
-
-            }
-            if (wybor == 3)
-            {
-                static int silnia(int x)
-                {
-                    if (x == 0) return 1;
-                    return silnia(x - 1) * x;
-                }
-                Console.WriteLine(silnia(liczba));
-                if (wybor == 4)
-                {
-                    break;
-                }
-            }
-
-        }
-    }
-}
-using System;
-
-public class Osoba
-{
-    private string imie;
-    private string nazwisko;
-    private int wiek;
-    private string pesel;
-
-    public Osoba(string imie, string nazwisko, int wiek, string pesel)
-    {
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-        Wiek = wiek;
-        this.pesel = pesel;
-    }
-
-    public string Imie
-    {
-        get { return imie; }
-        set { imie = value; }
-    }
-
-    public string Nazwisko
-    {
-        get { return nazwisko; }
-        set { nazwisko = value; }
-    }
-
-    public int Wiek
-    {
-        get { return wiek; }
-        set { wiek = value < 0 ? 0 : value; }
-    }
-
-    public string Pesel
-    {
-        get { return pesel; }
-    }
-
-    public string PrzedstawSie()
-    {
-        return $"Nazywam się {Imie} {Nazwisko} i mam {Wiek} lat.";
-    }
-}
-
-public class Licz
-{
-    private int wartosc;
-
-    public Licz(int wartosc)
-    {
-        this.wartosc = wartosc;
-    }
-
-    public void Dodaj(int liczba)
-    {
-        wartosc += liczba;
-    }
-
-    public void Odejmij(int liczba)
-    {
-        wartosc -= liczba;
-    }
-
-    public void WypiszStan()
-    {
-        Console.WriteLine($"Aktualна wartość: {wartosc}");
-    }
-}
-
-public class Sumator
-{
-    private int[] liczby;
-
-    public Sumator(int[] liczby)
-    {
-        this.liczby = liczby;
-    }
-
-    public int Suma()
-    {
-        int suma = 0;
-        foreach (int liczba in liczby)
-        {
-            suma += liczba;
-        }
-        return suma;
-    }
-
-    public int SumaPodziel3()
-    {
-        int suma = 0;
-        foreach (int liczba in liczby)
-        {
-            if (liczba % 3 == 0)
-            {
-                suma += liczba;
+                if (int.Parse(n) == 0) break;
             }
         }
-        return suma;
-    }
 
-    public int IleElementów()
-    {
-        return liczby.Length;
-    }
-
-    public void WypiszElementy()
-    {
-        Console.WriteLine("Elementy tablicy: " + string.Join(", ", liczby));
-    }
-
-    public void WypiszElementyWZakresie(int lowIndex, int highIndex)
-    {
-        for (int i = Math.Max(lowIndex, 0); i <= Math.Min(highIndex, liczby.Length - 1); i++)
+        static void WriteEven()
         {
-            Console.WriteLine($"Element[{i}] = {liczby[i]}");
+            Console.WriteLine("Podaj n");
+            var n = Console.ReadLine();
+
+            for (var i = 0; i < int.Parse(n); i += 2) // i = i+2;
+            {
+                Console.WriteLine(i);
+            }
         }
-    }
-}
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        // 1
-        Osoba osoba = new Osoba("Jan", "Kowalski", 30, "12345678901");
-        Console.WriteLine(osoba.PrzedstawSie());
-        osoba.Wiek = -5;
-        Console.WriteLine(
-            $"Age: {osoba.Wiek}");
 
-        //2
-        Licz licz = new Licz(10);
-        licz.Dodaj(5);
-        licz.Odejmij(3);
-        licz.WypiszStan();
+        static int Silnia(int x)
+        {
+            if (x == 0) return 1;
 
-        //3
-        int[] liczby = { 1, 2, 3, 6, 9, 12, 15 };
-        Sumator sumator = new Sumator(liczby);
-        Console.WriteLine($"sum {sumator.Suma()}");
-        Console.WriteLine(
-            $"Sum/ 3: {sumator.SumaPodziel3()}");
-        Console.WriteLine($"{sumator.IleElementów()}");
-        sumator.WypiszElementy();
-        sumator.WypiszElementyWZakresie(2, 10);
+            return Silnia(x - 1) * x;
+        }
+
+        static void CheckEven()
+        {
+            Console.WriteLine("Podaj liczbę");
+            var input = Console.ReadLine();
+
+            Console.WriteLine(IsEven(int.Parse(input)) ? "Parzysta" : "Nieparzysta");
+        }
+
+        static bool IsEven(int input)
+        {
+            return input % 2 == 0;
+        }
+
+        static void Game()
+        {
+            var random = new Random();
+            var numberToGuess = random.Next(100);
+            var counter = 0;
+            int guess;
+            do
+            {
+                counter++;
+                Console.WriteLine("Podaj liczbę od 0 do 100");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > numberToGuess)
+                    Console.WriteLine("Zgadywana liczba jest mniejsza");
+                if (guess < numberToGuess)
+                    Console.WriteLine("Zgadywana liczba jest większa");
+            }
+            while (guess != numberToGuess);
+
+            Console.WriteLine($"Zadłeś za {counter}");
+        }
     }
 }
---
+++ Lab2/Program.cs
@@ -0,0 +1,97 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+using Lab2.Models;
+
+Console.WriteLine("Hello, World!");
+
+
+Person os = new Person();
+
+os.Welcome();
+os.Welcome("DD");
+
+var n = new User { UserName = "Test" };
+n.UserName = "Test2";
+
+
+var os2 = Person.MapUser(n);
+
+
+Console.WriteLine(os2.Name);
+
+var sum = new Sumartor(new[] { 10, 20, 30, 40 });
+
+Console.WriteLine(sum.Count());
+Console.WriteLine(sum.SumDiv3());
+Console.WriteLine(sum.Sum());
+sum.Display(1, 2);
+
+*/
+
+
+/*
+Shape rec = new Rectangle();
+var rec2 = new Rectangle();
+
+
+var shapes = new List<Shape>()
+{
+    rec,
+    rec2,
+    new Shape()
+};
+*//*shapes.Add(rec);
+shapes.Add(rec2);
+shapes.Add(new Shape());*//*
+
+foreach (var shape in shapes)
+{
+    shape.Draw();
+}
+*/
+
+/*
+using Lab2.Models;
+
+var person = new Person();
+
+person.SetName("name");
+person.SetSurname("name");
+person.SetPesel("02261072763");
+
+Console.WriteLine(person.GetGender());
+Console.WriteLine(person.GetAge());*/
+
+
+
+using Lab2;
+using Lab2.Lab5;
+using Lab2.Lab6;
+
+//Shape shape = new Shape();
+Shape shape = new Square() {X = 5 };
+
+Console.WriteLine(shape.CalulateArea());
+
+
+IVehicle car = new Car();
+
+try
+{
+
+    car.Start();
+
+    car.Stop();
+}
+catch(NotImplementedException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+var book = new Book();
+
+Helper.SetCreationUser(book);
